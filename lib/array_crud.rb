99194642796f364b21790e8077e6a88f25ba0e8a
hugo valois <hvgaming@yahoo.com>
2018-08-27 23:23:33 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -27,11 +27,12 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  array=["wow", "I", "am", "really","arrays!"]
   array.index(2)
 end
 
 def retrieve_first_element_from_array(array)
+  
   array.index(0)
 end
 
