5da66da34d22e2473a7f984740c1beccce7049ec
hugo valois <hvgaming@yahoo.com>
2018-08-27 23:09:14 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -19,7 +19,7 @@ end
 
 def remove_element_from_end_of_array(array)
   game=["wow", "I", "am", "really","arrays!"]
-  game.pop("really")
+  game.pop()
 end
 
 def remove_element_from_start_of_array(array)
