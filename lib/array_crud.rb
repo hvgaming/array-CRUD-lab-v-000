aaf15a3b7b7b3c44048cf98fafc7c448f90c8e9c
hugo valois <hvgaming@yahoo.com>
2018-08-27 23:02:39 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -9,7 +9,7 @@ end
 
 def add_element_to_end_of_array(game,"arrays!")
   game=["wow", "I", "am", "really"]
-  game.unshift("arrays!")
+  game.shovel("arrays!")
 end
 
 def add_element_to_start_of_array(array, element)
