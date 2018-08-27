78122fe32278e1f58354c65b68b4fcda39b40562
hugo valois <hvgaming@yahoo.com>
2018-08-27 23:06:25 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -14,7 +14,7 @@ end
 
 def add_element_to_start_of_array(array, element)
   game=["wow", "I", "am", "really"]
-  game.unshift("")
+  game.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
