0a1d9f45fc02171b35fab03fe8a66eafa246ee1a
hugo valois <hvgaming@yahoo.com>
2018-08-28 00:38:46 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,12 +8,12 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array,element)
-   array = [ "I", "am", "really", "learning"]
+   
   array.push("arrays!")
 end
 
 def add_element_to_start_of_array(array, element)
-   array = ["I", "am", "really", "learning", "arrays!"]
+   
   array.unshift("wow")
 end
 
